dcff22c17e4aa9755d0beb8849973f0a36222475
AbhiRyan <abhi.ryan@yahoo.com>
2024-03-01 12:55:09 +1030

    update to test params

--- apicore/src/test/java/com/personalplugsite/apicore/ApiCoreApplicationTests.java
+++ apicore/src/test/java/com/personalplugsite/apicore/ApiCoreApplicationTests.java
@@ -1,10 +1,19 @@
 package com.personalplugsite.apicore;
 
+import com.personalplugsite.data.repos.TokenBlacklistRepo;
+import com.personalplugsite.data.repos.UserRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ApiCoreApplicationTests {
+
+  @MockBean
+  private TokenBlacklistRepo tokenBlacklistRepo;
+
+  @MockBean
+  private UserRepo userRepo;
 
   @Test
   void contextLoads() {
